ef1d52bb59ade6f6afb5cbeb2e144c02c5e8b311
songjoonhyuk <songjoonhyuk@gmail.com>
2015-11-12 08:48:42 +0000

    gem devise

--- app/controllers/ziptail_controller.rb
+++ app/controllers/ziptail_controller.rb
@@ -3,4 +3,7 @@ class ZiptailController < ApplicationController
     def index
     end
     
+    def regit
+    end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   
   root 'ziptail#index'
+  
+  get 'regit' => 'ziptail#regit'
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
